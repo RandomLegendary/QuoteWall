aed904a214338b577ae4fa9ed362b8e8689ebe49
Aidan Arijs <aidan.arijs@gmail.com>
2025-12-04 14:31:02 +0100

    Added Liking Feature

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ const nextButton = document.getElementById('next')
 const labelInput = document.getElementById('labelinput')
 const moreButton = document.getElementById('more')
 const backButton = document.getElementById('back')
+const likeButton = document.getElementById('like')
+const likeButtonExtra = document.getElementById('like-extra')
+const menu = document.getElementById('menu')
+const overMenu = document.getElementById('over-menu')
+const clear = document.getElementById('clear')
+
 
 let now = new Date();
 let start = new Date(now.getFullYear(), 0, 0);
@@ -28,6 +34,9 @@ if (now.getFullYear() == 2026) {
     submitButton.style.display = 'none'
     numberInput.style.display = 'none'
     labelInput.style.display = 'none'
+    likeButton.style.display = 'none'
+    clear.style.display = 'none'
+    menu.style.display = 'none'
 
 }
 
@@ -42,6 +51,7 @@ previousButton.addEventListener('click', function() {
         submit()
     } else {
         previousButton.style.display = 'none'
+        likeButtonExtra.style.display = 'none'
     }
 
 })
@@ -55,12 +65,15 @@ nextButton.addEventListener('click', function() {
 function submit() {
     inputId =  parseInt(numberInput.value)
     if (day === inputId){
+        likeButtonExtra.style.display = 'none'
         extraQuote.innerHTML = 'You forgot it already? Look above (:'
-    } else if (day >= inputId && inputId > 1) {
+    } else if (day >= inputId && inputId >= 1) {
         extraQuote.innerHTML = `${Quotes[inputId - 1]}`
         previousButton.style.display = 'inline-block'
         nextButton.style.display = 'inline-block'
+        likeButtonExtra.style.display = 'inline-block'
     } else {
+        likeButtonExtra.style.display = 'none'
         extraQuote.innerHTML = 'Choose a day earlier than today and above zero.'
     }
 }
@@ -81,6 +94,7 @@ moreButton.addEventListener('click', async function() {
     nextButton.style.display = 'none' 
     labelInput.style.display = 'none' 
     backButton.style.display = 'block'
+    likeButtonExtra.style.display = 'none'
 
     text.innerHTML = `${result.quote}`
     authorExtra.innerHTML = `-- ${result.author}`
@@ -105,3 +119,121 @@ backButton.addEventListener('click', function() {
         text.innerHTML =  "Quotes will be appearing here from January 1, 2026"
     }
 })
+
+let favoriteQuotes = []
+
+likeButton.addEventListener('click', function() {
+    if (!favoriteQuotes.includes(text.innerHTML)) {
+        favoriteQuotes.push(text.innerHTML)
+        localStorage.setItem("favoriteQuotes", JSON.stringify(favoriteQuotes))
+    }
+    
+})
+
+likeButtonExtra.addEventListener('click', function() {
+    if (!favoriteQuotes.includes(extraQuote.innerHTML)) {
+        favoriteQuotes.push(extraQuote.innerHTML)
+        localStorage.setItem("favoriteQuotes", JSON.stringify(favoriteQuotes))
+    }
+})
+
+let inMenu = false
+const menuDiv = document.getElementById('menu-div')
+
+
+menu.addEventListener('click', function() {
+
+    if (inMenu == false) {
+        text.style.display = 'none'
+        idNumber.style.display = 'none' 
+        submitButton.style.display = 'none'
+        numberInput.style.display = 'none' 
+        extraQuote.style.display = 'none' 
+        previousButton.style.display = 'none'
+        nextButton.style.display = 'none' 
+        labelInput.style.display = 'none' 
+        backButton.style.display = 'none'
+        likeButtonExtra.style.display = 'none'
+        likeButton.style.display = 'none'
+        moreButton.style.display = 'none'
+        authorExtra.style.display = 'none'
+        numberInput.value = ''
+        extraQuote.innerHTML = ''
+
+        menu.innerHTML = 'Return'
+        inMenu = true
+
+        overMenu.style.display = 'block'
+        menuDiv.style.display = 'block'
+
+
+        genLikedQuotes()
+
+
+    } else {
+        text.style.display = ''
+        idNumber.style.display = '' 
+        submitButton.style.display = ''
+        numberInput.style.display = '' 
+        labelInput.style.display = '' 
+        likeButton.style.display = ''
+        moreButton.style.display = ''
+        extraQuote.style.display = ''
+
+        menu.innerHTML = 'Menu'
+        inMenu = false
+
+        overMenu.style.display = 'none'
+        menuDiv.style.display = 'none'
+
+    }
+})
+
+const noQuotes = document.getElementById('no-quotes')
+
+function genLikedQuotes() {
+    let arrayOfQuotes = JSON.parse(localStorage.getItem('favoriteQuotes'))
+    console.log(arrayOfQuotes)
+
+    if (!arrayOfQuotes) {
+        noQuotes.style.display = 'block'
+        noQuotes.innerHTML = '<b>No Liked Quotes...</b>'
+        return;
+    } else {
+        noQuotes.style.display = 'block'
+        noQuotes.innerHTML = '<b>Click To Remove</b>'
+    }
+
+    menuDiv.innerHTML = ''
+    arrayOfQuotes.forEach(element => {
+        let el = document.createElement('button')
+        el.innerHTML = `${element}`
+        el.addEventListener('click', removeVAlue)
+        menuDiv.style.textAlign = 'center'
+        menuDiv.style.padding = '20px'
+        menuDiv.style.margin = '20px'
+        menuDiv.removeChild
+        menuDiv.appendChild(el)
+    });
+}
+
+clear.addEventListener('click', function() {
+    localStorage.clear()
+    genLikedQuotes()
+    menuDiv.innerHTML = ''
+})
+
+
+function removeVAlue() {
+    this.style.display = 'none'
+    let content = this.innerHTML
+
+    const index = favoriteQuotes.indexOf(content);
+
+    const x = favoriteQuotes.splice(index, 1);
+
+    // console.log(`myArray values: ${favoriteQuotes}`);
+    // console.log(`variable x value: ${x}`);
+
+    localStorage.setItem("favoriteQuotes", JSON.stringify(favoriteQuotes))
+}
